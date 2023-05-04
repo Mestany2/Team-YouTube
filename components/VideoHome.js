d6d665adf143e1c369e47e0187875ccc2ba9d486
Butler Steve <xry3440@hca.corpad.net>
2023-05-04 18:10:16 -0500

    who knows

--- components/VideoHome.js
+++ components/VideoHome.js
@@ -2,15 +2,15 @@ import PropTypes from 'prop-types';
 import Image from 'next/image';
 // eslint-disable-next-line import/no-extraneous-dependencies
 
-const VideoHome = ({ snippet }) => (
+const VideoHome = ({ id, title, thumbnail }) => (
   <>
     <div className="w-25">
       <div className="d-flex flex-column gap-1">
-        <Image className="rounded-2 " src={snippet.thumbnails.medium.url} width="300" height="120" />
+        <Image className="rounded-2 " src={thumbnail} width="300" height="120" />
         <div className="d-flex flex-nowrap gap-2 ms-2 px-2">
           <div className="p-2">
-            <h6 className="my-0">{snippet.title} </h6>
-            <p className="text-start">{snippet.channelTitle}</p>
+            <h6 className="my-0">{title} </h6>
+            <p className="text-start">{id}</p>
           </div>
         </div>
       </div>
@@ -21,20 +21,8 @@ const VideoHome = ({ snippet }) => (
 export default VideoHome;
 
 VideoHome.propTypes = {
-  id: PropTypes.shape({
-    videoId: PropTypes.string,
-  }).isRequired,
-  snippet: PropTypes.shape({
-    description: PropTypes.string,
-    channelTitle: PropTypes.string,
-    title: PropTypes.string,
-    thumbnails: PropTypes.shape({
-      medium: PropTypes.shape({
-        height: PropTypes.number,
-        width: PropTypes.number,
-        url: PropTypes.string,
-      }).isRequired,
-    }).isRequired,
-  }).isRequired,
+  id: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 
 };
--- pages/videoAPI.js
+++ pages/videoAPI.js
@@ -1,19 +1,22 @@
 import { useState, useEffect } from 'react';
-// import { v4 } from 'uuid';
+import { v4 } from 'uuid';
 import { getYTVideos } from '../api/videoData';
-// import VideoHome from '../components/VideoHome';
+import VideoHome from '../components/VideoHome';
 
 const VideoAPI = () => {
   const [videos, setVideos] = useState([]);
   useEffect(() => {
-    getYTVideos().then((data) => setVideos(data));
+    getYTVideos().then((data) => {
+      const videoArray = data[0];
+      setVideos(videoArray);
+    });
   }, []);
 
   // console.warn('VIDEOS: ', videos);
   return (
     <div className="d-flex flex-wrap gap-3 mt-5 ">
-      {videos.map((item) => console.warn('VideoItem: ', item))}
-      {/* {videos.map((video) => <VideoHome key={v4()} id={video.id} snippet={video.snippet} thumbnails={video.thumbnails} />)} */}
+      {videos.map((item) => console.warn('VideoItem: ', item.video))}
+      {videos.map((video) => <VideoHome key={v4()} id={video.videoId} title={video.title} thumbnail={video.thumbnails[0].url} />)}
       HELLO
 
     </div>
