52c0c4f3fef1d4d128024fbea3b72d36be79f225
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-05-04 20:44:20 -0500

    search

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { v4 }from 'uuid';
+import { v4 } from 'uuid';
 // import FilterComponent from '../components/FilterComponent';
 import { getYTVideos } from '../api/videoData';
 import VideoHome from '../components/VideoHome';
@@ -16,12 +16,10 @@ function Home({ query }) {
     });
   }, []);
 
-  const filteredVideos = videos.filter((oneVideo) => oneVideo.video.title.toLowerCase().includes(query.toLowerCase()));
-
   return (
     <div className="d-flex flex-wrap gap-3 ">
       {/* {categories.map((category) => <FilterComponent categories={category} />)} */}
-      {filteredVideos.map((item) => <VideoHome key={v4()} id={item.video.videoId} title={item.video.title} thumbnail={item.video.thumbnails[0].url} avatar={item.video.author.avatar[0].url} />)}
+      {videos.map((item) => <VideoHome key={v4()} id={item.video.videoId} title={item.video.title} thumbnail={item.video.thumbnails[0].url} avatar={item.video.author.avatar[0].url} />)}
 
     </div>
 
