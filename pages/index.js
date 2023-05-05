f5b474095a7345086506cc6afe7f04889ef9e6df
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-05-04 20:30:52 -0500

    Search ready

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
-import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+import { v4 }from 'uuid';
 // import FilterComponent from '../components/FilterComponent';
 import { getYTVideos } from '../api/videoData';
 import VideoHome from '../components/VideoHome';
 
-function Home() {
+function Home({ query }) {
 // const categories = ['Cats', 'Code', 'Music', 'Recently Uploaded'];
   const [videos, setVideos] = useState([]);
-  const query = 'coding';
+  // const q = 'coding';
   useEffect(() => {
     getYTVideos(query).then((data) => {
       const videoArray = data[0];
@@ -15,14 +16,25 @@ function Home() {
     });
   }, []);
 
+  const filteredVideos = videos.filter((oneVideo) => oneVideo.video.title.toLowerCase().includes(query.toLowerCase()));
+
   return (
     <div className="d-flex flex-wrap gap-3 ">
       {/* {categories.map((category) => <FilterComponent categories={category} />)} */}
-      {videos.map((item) => <VideoHome key={v4()} id={item.video.videoId} title={item.video.title} thumbnail={item.video.thumbnails[0].url} avatar={item.video.author.avatar[0].url} />)}
+      {filteredVideos.map((item) => <VideoHome key={v4()} id={item.video.videoId} title={item.video.title} thumbnail={item.video.thumbnails[0].url} avatar={item.video.author.avatar[0].url} />)}
 
     </div>
 
   );
 }
+
+Home.propTypes = {
+  query: PropTypes.string,
+
+};
+
+Home.defaultProps = {
+  query: 'coding',
+};
 
 export default Home;
