fc07bd0e9f26b2e1c1c6b7274326a1d348c98406
Steve Butler <butlersteve.c@gmail.com>
2023-05-10 15:44:36 -0500

    filter button

--- components/FilterComponent.js
+++ components/FilterComponent.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 // import { getAllVideos } from '../api/videoData';
 
 const FilterComponent = ({ category, setQuery, unfilteredVideos }) => {
   const [activeState, setActiveState] = useState(false);
+  const [buttonClick, setButtonClick] = useState(false);
 
   const handleButtonState = (e) => {
     if (e.target.value === 'ALL') {
       unfilteredVideos();
       setQuery('');
+      setButtonClick(true);
     } else {
       setQuery(e.target.value);
+      setButtonClick((prev) => !prev);
     }
-    setActiveState(true);
   };
+  useEffect(() => {
+    setActiveState((prev) => !prev);
+    setButtonClick(false);
+  }, [buttonClick]);
+
   return (
     <>
-      {activeState ? <Button variant="dark" className="px-3 fw-semi-bold fs-5 py-1 mb-3" value={category} onClick={handleButtonState}>{category}</Button>
+      {activeState ? <Button variant="dark" className="px-5 fw-semi-bold fs-5 py-1 mb-3" value={category} onClick={handleButtonState} active>{category}</Button>
         : <Button variant="light" className="px-5 fw-semi-bold fs-5 py-1 mb-3" value={category} onClick={handleButtonState}>{category}</Button>}
 
     </>
